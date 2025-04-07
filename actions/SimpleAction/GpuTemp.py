ea4aaedae98216aaa23511032f58cb1b194e4ada
yaoiprince <117023212+yaoiprince@users.noreply.github.com>
2025-04-07 07:03:02 +0000

    Update GpuTemp.py

---
+++ actions/SimpleAction/GpuTemp.py
@@ -0,0 +1,26 @@
+from src.backend.PluginManager.ActionBase import ActionBase
+from src.backend.PluginManager.PluginBase import PluginBase
+
+import os
+import psutil
+
+# Import gtk modules
+import gi
+gi.require_version("Gtk", "4.0")
+gi.require_version("Adw", "1")
+from gi.repository import Gtk, Adw
+
+class GPU(ActionBase):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.has_configuration = False
+    
+    def on_ready(self):
+        self.update()
+        
+    def on_tick(self):
+        self.update()
+
+    def update(self):
+        percent = round(psutil.gpu_percent())
+        self.set_center_label(text=f"{percent}%", font_size=24)
