89ce3f38e358d9301864a832aeeb8d73f9cb78ae
yaoiprince <117023212+yaoiprince@users.noreply.github.com>
2025-04-09 03:10:56 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,26 +1,34 @@
-# Import StreamController modules
+# import StreamController modules
 from src.backend.PluginManager.ActionBase import ActionBase
 from src.backend.DeckManagement.DeckController import DeckController
 from src.backend.PageManagement.Page import Page
 from src.backend.PluginManager.PluginBase import PluginBase
 
-# Import actions
+# import actions
 from .actions.SimpleAction.GpuTemp import Gpu
+
+# import pyNVML
+import nvidia_smi
+
+nvidia_smi.nvmlInit()
+handle = nvidia_smi.nvmlDeviceGetHandleByIndex(0)
+
+nvidia-smi -q –d CLOCK
 
 class PluginTemplate(PluginBase):
     def __init__(self):
         super().__init__()
 
-        ## Register actions
+        ## register actions
         self.simple_action_holder = ActionHolder(
             plugin_base = self,
             action_base = Gpu,
-            action_id = "dev_yaoiprince_GpuTemp::GpuTemp", # Change this to your own plugin id
+            action_id = "dev_yaoiprince_GpuTemp::GpuTemp", # change this to your own plugin id
             action_name = "Gpu Temperature",
         )
         self.add_action_holder(self.simple_action_holder)
 
-        # Register plugin
+        # register plugin
         self.register(
             plugin_name = "GpuTemp",
             github_repo = "https://github.com/yaoiprince/GpuTemp",
