043e6646a1b155ab1a44129580c117b03f9a0bdf
yaoiprince <117023212+yaoiprince@users.noreply.github.com>
2025-04-11 19:56:37 +0000

    Update NvidiaGpu.py

--- actions/SimpleAction/NvidiaGpu.py
+++ actions/SimpleAction/NvidiaGpu.py
@@ -7,9 +7,12 @@ from src.backend.PluginManager.PluginBase import PluginBase
 # Import python modules
 import nvidia-smi
 import subprocess
-import re
 
-nvidia_smi.nvmlInit() handle = nvidia_smi.nvmlDeviceGetHandleByIndex(0) 
+# Import gtk modules - used for the config rows
+import gi
+gi.require_version("Gtk", "4.0") 
+gi.require_version("Adw", "1") 
+from gi.repository import Gtk, Adw 
 
 class TemperatureSensor:
     def __init__(self):
@@ -23,11 +26,12 @@ class TemperatureSensor:
         self._gpu_temp = re.search(r"(\d+\.\d+)", result.stdout).group()
 
     def get_temperature_value(self):
-        return float(self._gpu_temp)  # Convert to a float for easier use
+        return self._gpu_temp
 
 if __name__ == "__main__":
     # Create an instance of the GPU Temperature class to get your GPU temperature
-    gpu_temp = TemperatureSensor()
+    gpu_temp = GPU Temperature()
+
 
 class GpuTemp(ActionBase):
     def __init__(self, *args, **kwargs):
