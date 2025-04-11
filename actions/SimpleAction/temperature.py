3e10ee1c3dbedd71ed7990410efcd8bbdc533315
yaoiprince <117023212+yaoiprince@users.noreply.github.com>
2025-04-11 21:10:12 +0000

    Update temperature.py

--- actions/SimpleAction/temperature.py
+++ actions/SimpleAction/temperature.py
@@ -5,8 +5,7 @@ from src.backend.PageManagement.Page import Page
 from src.backend.PluginManager.PluginBase import PluginBase 
 
 # Import python modules
-import nvidia-smi
-import subprocess
+import GPUtil
 
 # Import gtk modules - used for the config rows
 import gi
@@ -14,32 +13,18 @@ gi.require_version("Gtk", "4.0")
 gi.require_version("Adw", "1") 
 from gi.repository import Gtk, Adw 
 
-class TemperatureSensor:
-    def __init__(self):
-        self._gpu_temp = None
-
-    def get_temperature(self):
-        # Run the nvidia-smi command to get the GPU temperature
-        result = subprocess.run(['nvidia-smi', '--query=GPU_TEMPERATURE'], capture_output=True, text=True)
-
-        # Parse the output of the command and extract the temperature value
-        self._gpu_temp = re.search(r"(\d+\.\d+)", result.stdout).group()
-
-    def get_temperature_value(self):
-        return self._gpu_temp
-
-if __name__ == "__main__":
-    # Create an instance of the GPU Temperature class to get your GPU temperature
-    gpu_temp = GPU Temperature()
-
-
-class GpuTemp(ActionBase):
+class Temp(ActionBase):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.gpu_temp = TemperatureSensor()
 
     def on_ready(self) -> None:
-        print(f"{self.gpu_temp.get_temperature_value()}°C")
+    	GPUs = GPUtil.getGPUs()
+        
+        if not GPUs:  # Check if any GPU is available
+            print("Temp not found")
+        else:
+            for GPU in GPUs:
+                print(f"Temp: {GPU.temperature}"))
 
     def on_key_down(self) -> None:
         print("Key down") 
