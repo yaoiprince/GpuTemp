03edb204a81d10b8663e048b1b7bf866239b16ca
yaoiprince <117023212+yaoiprince@users.noreply.github.com>
2025-04-13 09:17:36 +0000

    Create backend.py

---
+++ backend/backend.py
@@ -0,0 +1,10 @@
+﻿from streamcontroller_plugin_tools import BackendBase 
+
+class Backend(BackendBase):
+    def __init__(self):
+        super().__init__()
+        
+        nvidia_smi.nvmlInit()
+        handle = nvidia_smi.nvmlDeviceGetHandleByIndex(0)
+
+backend = Backend()
