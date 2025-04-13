3fccf3c7b27bb62538083c6d27dc9f836f4125fc
yaoiprince <117023212+yaoiprince@users.noreply.github.com>
2025-04-13 09:25:51 +0000

    Update temperature.py

--- actions/SimpleAction/temperature.py
+++ actions/SimpleAction/temperature.py
@@ -1,8 +1,8 @@
 # Import StreamController modules
-from src.backend.PluginManager.ActionBase import ActionBase 
-from src.backend.DeckManagement.DeckController import DeckController 
-from src.backend.PageManagement.Page import Page 
-from src.backend.PluginManager.PluginBase import PluginBase 
+from src.backend.PluginManager.ActionBase import ActionBase
+from src.backend.DeckManagement.DeckController import DeckController
+from src.backend.PageManagement.Page import Page
+from src.backend.PluginManager.PluginBase import PluginBase
 
 # Import python modules
 import subprocess
@@ -11,28 +11,9 @@ import re
 ﻿﻿﻿﻿
 # Import gtk modules - used for the config rows
 import gi
-gi.require_version("Gtk", "4.0") 
-gi.require_version("Adw", "1") ﻿﻿﻿﻿
-from gi.repository import Gtk, Adw 
-
-class TemperatureSensor:
-    def __init__(self):﻿
-        self._gpu_temp = None
-
-    def get_temperature(self):
-        # Run the nvidia-smi command to get the GPU temperature
-        result = subprocess.run(['nvidia-smi', '--query=GPU_TEMPERATURE'], capture_output=True, text=True)
-﻿
-        # Parse the output of the command and extract the temperat﻿ure value
-        self._gpu_temp = r﻿e.sea﻿rch(r"(\d+\.\d+)", result.st﻿d﻿﻿out).group()
-
-    def get_temperature_value(self):
-        return self._gpu_temp
-
-if __name__ == "__main__":
-    # Create an instance of the GPU Temperature class to get your GPU temperature
-    gpu_temp = GPU Temperature()
-﻿
+gi.require_version("Gtk", "4.0")
+gi.require_version("Adw", "1")
+from gi.repository import Gtk, Adw
 
 class Temp(ActionBase):
     def __init__(self, *args, **kwargs):
@@ -43,7 +24,7 @@ class Temp(ActionBase):
         print(f"{self.gpu_temp.get_temperature_value()}°C")
 
     def on_key_down(self) -> None:
-        print("Key down") 
+        print("Key down")
 
     def on_key_up(self) -> None:
         print("Key up")
