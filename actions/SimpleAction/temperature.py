722833f8fd8d2e28732ae944a174651eed3daf50
yaoiprince <117023212+yaoiprince@users.noreply.github.com>
2025-04-13 09:22:32 +0000

    Update temperature.py

--- actions/SimpleAction/temperature.py
+++ actions/SimpleAction/temperature.py
@@ -7,23 +7,24 @@ from src.backend.PluginManager.PluginBase import PluginBase
 # Import python modules
 import subprocess
 import nvidia_smi
-
+import re
+﻿﻿﻿﻿
 # Import gtk modules - used for the config rows
 import gi
 gi.require_version("Gtk", "4.0") 
-gi.require_version("Adw", "1") 
+gi.require_version("Adw", "1") ﻿﻿﻿﻿
 from gi.repository import Gtk, Adw 
 
 class TemperatureSensor:
-    def __init__(self):
+    def __init__(self):﻿
         self._gpu_temp = None
 
     def get_temperature(self):
         # Run the nvidia-smi command to get the GPU temperature
         result = subprocess.run(['nvidia-smi', '--query=GPU_TEMPERATURE'], capture_output=True, text=True)
-
-        # Parse the output of the command and extract the temperature value
-        self._gpu_temp = re.search(r"(\d+\.\d+)", result.stdout).group()
+﻿
+        # Parse the output of the command and extract the temperat﻿ure value
+        self._gpu_temp = r﻿e.sea﻿rch(r"(\d+\.\d+)", result.st﻿d﻿﻿out).group()
 
     def get_temperature_value(self):
         return self._gpu_temp
@@ -31,7 +32,7 @@ class TemperatureSensor:
 if __name__ == "__main__":
     # Create an instance of the GPU Temperature class to get your GPU temperature
     gpu_temp = GPU Temperature()
-
+﻿
 
 class Temp(ActionBase):
     def __init__(self, *args, **kwargs):
