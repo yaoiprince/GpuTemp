cd3e316b538c90f30a86b2451ba134fe373e80a1
yaoiprince <117023212+yaoiprince@users.noreply.github.com>
2025-04-21 09:52:33 +0000

    Update temperature.py

--- actions/SimpleAction/temperature.py
+++ actions/SimpleAction/temperature.py
@@ -3,6 +3,7 @@ from src.backend.PluginManager.ActionBase import ActionBase
 from src.backend.PluginManager.PluginBase import PluginBase
 from src.backend.PluginManager.ActionHolder import ActionHolder
 
+import pynvml
 from nvidia_ml_py import nvidia_smi
 import subprocess
 
@@ -12,13 +13,16 @@ gi.require_version("Gtk", "4.0")
 gi.require_version("Adw", "1")
 from gi.repository import Gtk, Adw
 
+nvidia_smi.nvmlInit()
+handle = nvidia_smi.nvmlDeviceGetHandleByIndex(0)
+
 class TemperatureSensor:
     def get_temperature_value(self):
         # Execute nvidia-smi to fetch GPU temperature
-        result = subprocess.run(['nvidia-smi', '-q -d TEMPERATURE'], capture_output=True, text=True)
+        result = subprocess.run(['nvidia-smi', '-q -d temperature'], capture_output=True, text=True)
 
         if result.returncode != 0:
-            raise Exception(f"Failed to execute nvidia-smi: {result.stderr}")
+            raise Exception(f"nvsmi failed: {result.stderr}")
 
         gpu_temp = float(re.search(r"(\d+\.\d+)", result.stdout).group())
         return gpu_temp
@@ -30,14 +34,12 @@ class temp(ActionBase):
 
     def on_ready(self) -> None:
         try:
-            gpu_temp = self.gpu_temp_sensor.get_temperature_value()
             print(f"{gpu_temp}°C")
         except Exception as e:
             print(e)
 
     def on_key_down(self) -> None:
         try:
-            gpu_temp = self.gpu_temp_sensor.get_temperature_value()
             print(f"GPU Temperature: {gpu_temp}°C")
             # You can also display this temperature in the StreamController UI if available
         except Exception as e:
